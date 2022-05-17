6a97d705997518efaad3cb51537fc4da58c20621
AliHossain01 <alihossaincse01@gmail.com>
2022-05-17 19:33:24 +0600

    module 71

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonials from './Testimonials';
+import Contact from './Contact';
+import Footer from '../Shared/Footer';
 
 const Home = () => {
     return (
         <div>
-
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const About = () => {
+const Login = () => {
     return (
         <div>
-            <h2>About</h2>
+            <h2>Login</h2>
         </div>
     );
 };
 
-export default About;
+export default Login;
