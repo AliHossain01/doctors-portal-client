200bf1e9e841568fada71518a189a7590b757622
AliHossain01 <alihossaincse01@gmail.com>
2022-05-21 01:12:01 +0600

    specific admin

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = () => {
 
     const logout = () => {
         signOut(auth);
+        localStorage.removeItem('accessToken');
     };
 
     const menuItems = <>
@@ -21,7 +22,7 @@ const Navbar = () => {
         {
             user && <li><Link to="/dashboard">Dashboard</Link></li>
         }
-        <li>{user ? <button className="btn btn-ghost" onClick={logout} >Sign Out</button> : <Link to="/login">Login</Link>}</li>
+        <li>{user ? <button className="btn btn-ghost" onClick={logout} >{user.displayName} Sign Out</button> : <Link to="/login">Login</Link>}</li>
     </>
     return (
         <div className="navbar bg-base-100">
